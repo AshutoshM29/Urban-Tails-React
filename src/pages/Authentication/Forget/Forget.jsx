a0b583ae84ffafd4245746a1878c312220a4cc2a
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-04-01 12:11:51 +0530

    feat: added forget page

--- src/pages/Authentication/Forget/Forget.jsx
+++ src/pages/Authentication/Forget/Forget.jsx
@@ -1,6 +1,32 @@
+import '../authentication.css'
+import {forget} from '../../../assets/images'
+import { Link } from 'react-router-dom'
+
 function Forget() {
     return (
-      <div>Forget</div>
+      <div>  <main class="main-authentication">
+      <section>
+          <form class="form-authentication">
+              <h2 class="form-title">Forget Password</h2>
+              <label class="label" for="email">Enter your email address</label>
+              <input class="input-outline" type="email" name="email" placeholder="urbantails.com" required />
+              <p>OR</p>
+              <label class="label" for="mobile-no">Enter your Mobile Number</label>
+              <input class="input-outline" type="number" name="mobile-no" placeholder="0123456789" required />
+
+              <div class="section-btn-authentication">
+                  <button class="btn btn-primary-solid">
+                      Submit
+                  </button>
+                  <p>Remember your password?<Link class="underline" to="/Login"><span class="span-link"> Cancel</span></Link></p>
+              </div>
+          </form>
+      </section>
+      <div class="hero-login">
+          <img src={forget} alt="Forgot Password" />
+      </div>
+
+  </main></div>
     )
   }
   
