968e196b659e92d814b23741c9d79789b78c6455
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-04-01 12:12:22 +0530

    feat: added logout page

--- src/pages/Authentication/Logout/Logout.jsx
+++ src/pages/Authentication/Logout/Logout.jsx
@@ -1,6 +1,23 @@
+import '../authentication.css'
+import {logout} from '../../../assets/images'
+import { Link } from 'react-router-dom'
+
 function Logout() {
     return (
-      <div>Logout</div>
+      <div> <main class="main-authentication">
+      <section>
+          <form class="form-authentication">
+                  <h2 class="form-title">Logged Out</h2>
+                  <p class="body-cp-md">You have been successfully logged out !</p>
+                  <div class="section-btn-authentication">
+                      <p>Already have an account!! <Link class="underline" to="/Login"><span class="span-link"> Log In</span></Link></p>
+                  </div>
+          </form>
+      </section>
+      <div class="hero-logout">
+          <img src={logout} alt="Log In" />
+      </div>
+  </main></div>
     )
   }
   
