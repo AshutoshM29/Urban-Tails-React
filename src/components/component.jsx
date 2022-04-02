47893547b4e2bcccd027bbd8b14f8c788ba2415f
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-04-02 12:15:22 +0530

    feat: added sidebar

--- src/components/component.jsx
+++ src/components/component.jsx
@@ -1,3 +1,4 @@
 export {Navigation} from "./Navigation/Navigation";
 export {Footer} from "./Footer/Footer";
-export {Sidebar} from "./Sidebar/Sidebar"
+export {Sidebar} from "./Sidebar/Sidebar";
+export {CardVertical} from "./Card/Card-Vertical/Card-Vertical";
