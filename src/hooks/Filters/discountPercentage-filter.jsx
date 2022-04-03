65c8bfbb1a17b4d2e5ba7f702988021dd1dcc682
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-04-03 21:36:49 +0530

    Create discountPercentage-filter.jsx

---
+++ src/hooks/Filters/discountPercentage-filter.jsx
@@ -0,0 +1,9 @@
+
+export function discountedItems(product, value) {
+    if (value) {
+        return product.filter((items) => {
+            return items.discountedPrice >= value
+        })
+    }
+    return product
+}
