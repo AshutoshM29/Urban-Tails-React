a3deb28044f0e25b721074f967d7102c700f98fd
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-04-03 22:03:45 +0530

    fix indentation

--- src/hooks/Filters/filter.jsx
+++ src/hooks/Filters/filter.jsx
@@ -6,7 +6,14 @@ export { discountedItems } from "./discountPercentage-filter"
 export { SortedProducts } from "./sort_priceFilter"
 export { useSortedProduct } from "./product-context"
 export {
-    clearFilterHandler, discountHandler, priceHighToLowHandler,
-    priceLowToHighHandler, sortByCategoryHandler, priceSortHandler,
-    stars4Handler, stars3Handler, stars2Handler, stars1Handler
+    clearFilterHandler,
+    discountHandler,
+    priceHighToLowHandler,
+    priceLowToHighHandler,
+    sortByCategoryHandler,
+    priceSortHandler,
+    stars4Handler,
+    stars3Handler,
+    stars2Handler,
+    stars1Handler
 } from "./filterControllers"
