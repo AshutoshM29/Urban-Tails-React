8e702db1082cd1deb1c8fa766d44dd163b4931a3
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:34:30 +0530

    Update Dog-Category.jsx

--- src/pages/Homepage/Dog-Category.jsx
+++ src/pages/Homepage/Dog-Category.jsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import React from 'react';
 import { useCategory } from "../../hooks/useCategory";
-import "./homepage.css"
+import { useNavigate } from "react-router-dom";
+import { useDataLayer } from "../../context";
+import { useEffect } from "react";
+import "./Homepage.css"
+
+
 const Categories=()=>{
 const {categories}=useCategory();
+const currentLocation = useNavigate();
+    const { getListOfProducts } = useDataLayer();
+  
+    useEffect(() => {
+      getListOfProducts();
+    }, []);
 
 return ( <div className="section-category">
     { categories.map((categories)=>{
     return (
-    <Link to="/Product">
-    <img className="img-category" src={`${categories.categoryImageDog}`} alt={`${categories.categoryName}`} />
-    <p className="desc-category">{categories.titleDog}</p>
-    </Link>
+    <button className="btn-product" onClick={() => currentLocation("/Product")}>
+        <img className="img-category" src={`${categories.categoryImageDog}`} alt={`${categories.categoryName}`} />
+        <p className="desc-category">{categories.titleDog}</p>
+    </button>
     )
     })
     }
