017651c23b1955650ff19b2c7331b2e87e8a72ec
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:50:16 +0530

    feat: added toastify

---
+++ src/custom-hooks/useToast.jsx
@@ -0,0 +1,23 @@
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+
+const useToast = () => {
+  const showToast = (toastDescription, toastType) => {
+    const notify = () => {
+      toast[toastType](toastDescription, {
+        theme: "light",
+        position: "bottom-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    };
+    notify();
+  };
+  return { showToast };
+};
+
+export { useToast };
