a50dec79a179868410f4761429f37131595c49ec
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:49:56 +0530

    feat: added authentication services

--- src/services/index.js
+++ src/services/index.js
@@ -1,25 +1,20 @@
 export { userLoginService } from "./authServices/userLoginService.jsx";
 export { userSignupService } from "./authServices/userSignupService.jsx";
 export {
-  getAllItemsOfWishlist,
-  addProductToWishlist,
-  removeProductFromWishlist,
-} from "./wishlistServices/wishlistServices.jsx";
-export {
   getAllProducts,
   getSingleProduct,
 } from "./productServices/productServices.jsx";
 export {
-  getAllCategoriesService,
-  getSingleCategoryService,
-} from "./categoryServices/categoryServices.jsx";
+  getAllItemsOfWishlist,
+  addProductToWishlist,
+  removeProductFromWishlist,
+} from "./wishlistServices/wishlistServices.jsx";
 export {
   getAllItemOfCartService,
   addNewItemTocart,
   removeItemFromCart,
 } from "./cartServices/cartServices.jsx";
 export {
-  postAddressService,
-  getAddressService,
-  deleteAddressService,
-} from "./addressServices/addressServices.jsx";
+  getAllCategoriesService,
+  getSingleCategoryService,
+} from "./categoryServices/categoryServices.jsx";
