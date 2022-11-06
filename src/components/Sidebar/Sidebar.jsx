8ad7585a60eab6145617b122b0e98a89d17cb392
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:27:09 +0530

    update: sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,146 +1,170 @@
-import './sidebar.css'
-import { useState } from "react";
-import { clearFilterHandler, discountHandler, priceHighToLowHandler, priceLowToHighHandler, sortByCategoryHandler, rating1Handler, rating2Handler, rating3Handler, rating4Handler, useSortedProduct } from "../../hooks/Filters/filter";
+import React from 'react';
+import { useEffect } from "react";
+import "./Sidebar.css";
+import { useDataLayer } from "../../context/index";
 
-export const Sidebar = () => {
-    const { state,dispatch } = useSortedProduct();
-    return (<aside className="section-filter">
+const Sidebar = () => {
+  const {
+    state,
+    dispatch,
+    rangePrice,
+    setRangePrice,
+    category,
+    sortType,
+    setSortType,
+    rating,
+    setRating,
+    searchTerm,
+    handlChangeChecked,
+    setCategory,
+    initialCategoryState,
+    handlRadioChangeChecked,
+    filterByRatingsDB,
+    handleSortRadioChangeChecked,
+    sortByPriceDB,
+  } = useDataLayer();
 
-        <div className="flex">
-            <h3 className="header-filter">Filter</h3>
-            <label className="switch">
-                <input onChange={()=>clearFilterHandler(dispatch)} type="checkbox" />
-                <span className="slider round"></span>
+  const handleClearFilter = () => {
+    setRating(filterByRatingsDB);
+    setSortType(sortByPriceDB);
+    setRangePrice(999);
+    setCategory(initialCategoryState);
+  };
+
+  useEffect(() => {
+    const applyFilters = () => {
+      let filteredData = state.productData;
+      const categoriesChecked = category
+        .filter((item) => item.checked)
+        .map((item) => item.label.toLowerCase());
+
+      if (categoriesChecked.length) {
+        filteredData = filteredData?.filter((item) =>
+          categoriesChecked.includes(item.categoryName)
+        );
+      }
+      if (rangePrice) {
+        filteredData = filteredData?.filter(
+          (item) => item.discountedPrice < rangePrice
+        );
+      }
+
+      const checkedRating = rating.find((item) => item.checked);
+      if (checkedRating) {
+        filteredData = filteredData?.filter(
+          (item) => item.rating >= Number(checkedRating.id)
+        );
+      }
+      if (searchTerm) {
+        filteredData = filteredData.filter(
+          (item) =>
+            item.name.toLowerCase().search(searchTerm.toLowerCase().trim()) !==
+            -1
+        );
+      }
+
+      const checkedSortingType = sortType.find((item) => item.checked);
+      if (checkedSortingType) {
+        filteredData =
+          checkedSortingType.id === 2
+            ? filteredData.sort((a, b) => b.discountedPrice - a.discountedPrice)
+            : filteredData.sort(
+                (a, b) => a.discountedPrice - b.discountedPrice
+              );
+      }
+
+      dispatch({ type: "SET_FILTERED_DATA", payload: filteredData });
+    };
+    applyFilters();
+  }, [rating, rangePrice, category, sortType, searchTerm]);
+
+return (
+  <aside className="section-filter">
+    <div className="flex">
+      <h3 className="header-filter">Filter</h3>
+        <button className="btn btn-error-solid" onClick={() => handleClearFilter()}>
+          Clear
+        </button>
+    </div>
+    <h4 className="sub-header-filter">Price</h4>
+    <input
+      className="slider-custom"
+      step="100"
+      type="range"
+      min="99"
+      max="999"
+      value={rangePrice}
+      onChange={(e) => setRangePrice(e.target.value)}
+    />
+    <label className="price-range">
+      <span id="price-max">₹99</span>
+      <span id="price-min">₹499</span>
+      <span id="price-mid">₹999</span>
+    </label>
+    <br/>
+    <hr/>
+    <h4 className="sub-header-filter">Ratings</h4>
+      {rating.map((item) => {
+        return (
+          <div className="container-checkbox" key={item.id}>
+            <input
+              className="radio"
+              type="radio"
+              name="star"
+              id={item.id}
+              onChange={() => handlRadioChangeChecked(item.id)}
+              value={item.id}
+              checked={item.checked}
+            />
+            <label htmlFor={item.id} className="input-desc">
+              {item.label}
             </label>
-        </div>
-        <br/>
-        
-                <h4 className="sub-header-filter">Price</h4>
+          </div>
+        );
+      })}
+    <br/>
+    <hr/>
+    <h4 className="sub-header-filter">Life Stages</h4>
+      {category.map((item) => {
+        return (
+          <div className="container-checkbox" key={item.id}>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name={item.label}
+              id={item.id}
+              checked={item.checked}
+              onChange={() => handlChangeChecked(item.id)}
+            />
+            <label htmlFor={item.label} className="input-desc">
+              {item.label}
+            </label>
+          </div>
+        );
+      })}
+    <br/>
+    <hr/>
+      <h4 className="sub-header-filter">Sort Price</h4>
+        {sortType.map((item) => {
+          return (
+            <div className="container-checkbox" key={item.id}>
+              <input
+                className="checkbox"
+                type="radio"
+                name="price"
+                id={item.id}
+                onChange={() => handleSortRadioChangeChecked(item.id)}
+                value={item.id}
+                checked={item.checked}
+              />
+              <label htmlFor="low-to-high" className="input-desc">
+                Price - {item.label}
+              </label>
+            </div>
+          );
+        })}
+  </aside>
+  );
+};
 
-                    <input
-                        className="slider-custom"
-                        step="100"
-                        type="range"
-                        min="99"
-                        max="999"
-                        checked={state.discountValue}
-                        onChange={(e)=>discountHandler(e,dispatch)}
-                    />
-                    <label className="price-range">
-                        <span id="price-max">₹99</span>
-                        <span id="price-min">₹499</span>
-                        <span id="price-mid">₹999</span>
-                    </label>
-                    <br/>
-                    <hr/>
-
-                    <h4 className="sub-header-filter">Ratings</h4>
-                    <div className='checkbox'>
-                        <input
-                            className="input-checkbox"
-                            type="checkbox"
-                            id="rating"
-                            checked={state.rating4}
-                            onChange={()=>rating4Handler(dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="rating" >4 Stars & above</label>
-                    </div>
-                    <div className='checkbox'>
-                        <input
-                            className="input-checkbox"
-                            type="checkbox"
-                            id="rating"
-                            checked={state.rating3}
-                            onChange={()=>rating3Handler(dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="rating" >3 Stars & above</label>
-                    </div>
-                    <div className='checkbox'>
-                        <input
-                            className="input-checkbox"
-                            type="checkbox"
-                            id="rating"
-                            checked={state.rating2}
-                            onChange={()=>rating2Handler(dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="rating" >2 Stars & above</label>
-                    </div>
-                    <div className='checkbox'>
-                        <input
-                            className="input-checkbox"
-                            type="checkbox"
-                            id="rating"
-                            checked={state.rating1}
-                            onChange={()=>rating1Handler(dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="rating" >1 Stars & above</label>
-                    </div>
-                    <br/>
-                    <hr/>
-
-                    <h4 className="sub-header-filter">Life Stages</h4>
-                    <div className="radio">
-                        <input
-                            type="radio"
-                            id="puppy"
-                            name="category"
-                            value="puppy"
-                            checked={state.sortBy === "puppy"}
-                            onChange={(e)=>sortByCategoryHandler(e,dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="puppy">Puppy</label>
-                    </div>
-                    <div className="radio">
-                        <input  
-                            type="radio"
-                            id="adult"
-                            name="category"
-                            value="adult"
-                            checked={state.sortBy === "adult"}
-                            onChange={(e)=>sortByCategoryHandler(e,dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="adult">Adult</label>
-                    </div>
-                    <div className="radio">
-                        <input 
-                            type="radio"
-                            id="senior"
-                            name="category"
-                            value="senior"
-                            checked={state.sortBy === "senior"}
-                            onChange={(e)=>sortByCategoryHandler(e,dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="senior">Senior</label>
-                    </div>
-                    <br/>
-                    <hr/>
-
-                    <h4 className="sub-header-filter">Sort Price</h4>
-                    <div className="radio">
-                        <input
-                            className="category"
-                            type="radio"
-                            id="price_sort"
-                            name="price_sort"
-                            value="price_sort"
-                            checked={state.priceHighLow === "low-2-high"}
-                            onChange={()=>priceHighToLowHandler(dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="price_sort">Low to high</label>
-                    </div>
-                    <div className="radio">
-                        <input
-                            className="category"
-                            type="radio"
-                            id="price_sort"
-                            name="price_sort"
-                            value="price_sort"
-                            checked={state.priceHighLow === "high-2-low"}
-                            onChange={()=>priceLowToHighHandler(dispatch)}
-                        />
-                        <label className="input-desc" htmlFor="high-2-low">High to low</label>
-                    </div>
-
-                <br/>
-    </aside>)
-}
+export { Sidebar };
