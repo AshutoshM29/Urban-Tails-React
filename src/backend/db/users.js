a53131732722225eabbb52507db97f7c0f43545e
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:12:45 +0530

    feat: added user data

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
+    firstName: "Ashutosh",
+    lastName: "Mishra",
+    email: "ashutoshrm01@gmail.com",
+    password: "Ashu1234",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
