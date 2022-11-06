ddcbe5fb8c8dea2ad0d1122c59f1dca5cbda7871
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:49:41 +0530

    feat: added Order conformation page

---
+++ src/pages/OrderPlaced/OrderPlaced.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { orderPlaced } from "../../assets/images";
+import "./OrderPlaced.css";
+import { Footer, Navigation } from "../../components";
+
+const OrderPlaced = () => {
+
+return (
+<div>
+  <Navigation />
+  <main className="main-authentication">
+    <section className="section-info">
+      <h2>Order placed successfully!!</h2>
+      <Link to="/" className="btn btn-primary-solid">
+        Home
+      </Link>
+    </section>
+    <div className="hero-login">
+      <img src={orderPlaced} alt="Log In" />
+    </div>
+  </main>
+  <Footer />
+</div>
+);
+};
+
+export { OrderPlaced };
