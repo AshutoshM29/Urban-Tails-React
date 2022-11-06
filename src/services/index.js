0ae6e513648651df0d1b13a9d6f50bf7832dcf54
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:43:54 +0530

    Create index.js

---
+++ src/services/index.js
@@ -0,0 +1,25 @@
+export { userLoginService } from "./authServices/userLoginService.jsx";
+export { userSignupService } from "./authServices/userSignupService.jsx";
+export {
+  getAllItemsOfWishlist,
+  addProductToWishlist,
+  removeProductFromWishlist,
+} from "./wishlistServices/wishlistServices.jsx";
+export {
+  getAllProducts,
+  getSingleProduct,
+} from "./productServices/productServices.jsx";
+export {
+  getAllCategoriesService,
+  getSingleCategoryService,
+} from "./categoryServices/categoryServices.jsx";
+export {
+  getAllItemOfCartService,
+  addNewItemTocart,
+  removeItemFromCart,
+} from "./cartServices/cartServices.jsx";
+export {
+  postAddressService,
+  getAddressService,
+  deleteAddressService,
+} from "./addressServices/addressServices.jsx";
