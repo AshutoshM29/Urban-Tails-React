6e87bfe148a8cedcd4450a30d0c4273ffa38476a
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:23:11 +0530

    feat: added components

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export { CartCard } from "./Card/Card-Cart/CartCard";
+export { HorizontalCard } from "./Card/Card-Horizontal/HorizontalCard";
+export { PriceCard } from "./Card/Card-Price/PriceCard";
+export { VerticalCard } from "./Card/Card-Vertical/VerticalCard";
+export { Footer } from "./Footer/Footer";
+export { Navigation } from "./Navigation/Navigation";
+export { RequiresAuth } from "./RequiresAuth/RequiresAuth";
+export { Sidebar } from "./Sidebar/Sidebar";
