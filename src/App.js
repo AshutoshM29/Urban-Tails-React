b5656a208446ecb97767a2c7d6b86c23340dc032
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-06 19:02:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
+import React from 'react';
 import "./App.css";
-import {Routes, Route} from "react-router-dom";
-import { Navigation,Footer } from "./components/component";
-import {Homepage, Product, Cart, Wishlist, Forget, Login, Logout, Signup} from "./pages/pages";
-import Mockman from "mockman-js";
+import {
+  Homepage,
+  Product,
+  Wishlist,
+  Cart,
+  Login,
+  Logout,
+  Signup,
+  OrderPlaced,
+  Profile,
+  NotFound,
+  Forget,
+} from "./pages";
+import { RequiresAuth } from "./components";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <div className="main-style">
       <Routes>
-        <Route path="*" element={<Homepage/>}></Route>
-        <Route path="/product" element={<Product/>}></Route>
-        <Route path="/cart" element={<Cart/>}></Route>
-        <Route path="/wishlist" element={<Wishlist/>}></Route>
-        <Route path="/forget" element={<Forget/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/logout" element={<Logout/>}></Route>
-        <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="/mock" element={<Mockman/>}></Route>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Product" element={<Product />} />
+        <Route
+          path="/Wishlist"
+          element={
+            <RequiresAuth>
+              <Wishlist />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/Cart"
+          element={
+            <RequiresAuth>
+              <Cart />
+            </RequiresAuth>
+          }
+        />
+        <Route path="/Forget" element={<Forget />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Logout" element={<Logout />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/OrderPlaced" element={<OrderPlaced />} />
+        <Route
+          path="/Profile"
+          element={
+            <RequiresAuth>
+              <Profile />
+            </RequiresAuth>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      </div>
-      <div>
-    <Footer />
-    </div>
     </div>
   );
 }
