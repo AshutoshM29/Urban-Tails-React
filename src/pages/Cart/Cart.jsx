2f723bc002094e24307695384906d034c5a6af33
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-12-06 13:44:10 +0530

    fix: import

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Footer, PriceCard, CartCard, Navigation } from "../../components";
 import { useDataLayer } from "../../context";
 import { Link } from "react-router-dom";
-import "./Cart.css"
+import "./cart.css"
 
 const Cart = () => {
   const { state } = useDataLayer();
